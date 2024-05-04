7c5cc488c4fb0f3546578a35aa8ca2fc4c20757a
MonaMaNotAvailable <zma4@wpi.edu>
2024-05-03 23:50:20 -0400

    generate plot for final report

--- DataVisualizations/Scripts/laplace.py
+++ DataVisualizations/Scripts/laplace.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import laplace
 
 # Increase the overall font size
-plt.rcParams.update({'font.size': 17})  # Adjust the size as needed
+plt.rcParams.update({'font.size': 16})  # Adjust the size as needed
 
 # Set up the mean and calculate sigmas from variances
 mean = 59 
@@ -11,8 +11,8 @@ variance_1 = 1706.20527399
 epsilon_1 = 2.02
 sigma_1 = np.sqrt(variance_1 / 2)
 
-variance_2 = 1706.20527399
-epsilon_2 = 2.02
+variance_2 = 69.62
+epsilon_2 = 10
 sigma_2 = np.sqrt(variance_2 / 2)
 
 # Set the range for x based on the larger sigma to ensure both distributions are visible
